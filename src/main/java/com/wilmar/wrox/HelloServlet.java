406ac08b31ff13f4db0c89ff41ca47567c9debae
wilmar <wil.pac510@gmail.com>
2021-08-30 02:02:18 -0500

    Cargando Servlet para vista de Checkboxes

--- src/main/java/com/wilmar/wrox/HelloServlet.java
+++ src/main/java/com/wilmar/wrox/HelloServlet.java
@@ -9,12 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
 		response.getWriter().println("Hello, World!");
 	}
